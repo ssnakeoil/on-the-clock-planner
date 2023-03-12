f0cf8c82b3242c2ea72e049ebbda3570d719b72a
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2023-03-12 00:53:07 -0500

    add code for date

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,9 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  //display current day
+  $("#currentDay").text(dayjs().format("dddd, DD MMMM YYYY"));
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
